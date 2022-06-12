686cfb6ddbb6f5706711ea64c1d29908ac4dfe21
pkoziel1 <74543639+pkoziel1@users.noreply.github.com>
2022-06-12 11:25:18 +0200

    updated algorithm to run in loop

--- fast_unfolding.py
+++ fast_unfolding.py
@@ -1,17 +1,8 @@
-from typing import Dict, List, Iterable
-
-import numpy
-import networkx as nx
-from networkx import Graph
-import random
-import networkx.algorithms.community as nx_comm
+from typing import Dict, Iterable
 from graph import KozikGraph, generate_child_kozik_graph
 
 
 class FastUnfolding:
-    def __init__(self):
-        ...
-
     def run(self, graph: KozikGraph):
         while True:
             initial_modularity = graph.modularity()
@@ -20,67 +11,33 @@ class FastUnfolding:
                 break
             graph = generate_child_kozik_graph(graph)
         print(f'Modularity = {graph.modularity()}')
+        print('stop')
 
     def process_graph(self, graph: KozikGraph):
-        self._assign_init_communities(graph)
-        nodes = [node for node in graph.get_nodes()]
-        # random.shuffle(nodes)
-        print('stop')
-        for node in nodes:
+        for node in graph.get_nodes():
             neighbours = graph.get_neighbours(node)
-            neighbour_to_gain_map: Dict[int, float] = self.map_modularity_gain_to_neighbours(graph, node, neighbours)
+            neighbour_to_gain_map: Dict[int, float] = self.map_modularity_gain_to_neighbours(
+                graph, node, neighbours)
             highest_gain_neighbour = max(neighbour_to_gain_map, key=neighbour_to_gain_map.get)
-            # print(neighbour_to_gain_map[highest_gain_neighbour])
-            if neighbour_to_gain_map[highest_gain_neighbour] > 0: #parametr
+            if neighbour_to_gain_map[highest_gain_neighbour] > 0:
                 new_community = graph.get_node_community(highest_gain_neighbour)
                 graph.add_to_community(new_community, node)
-            print('stop')
-        print('stop')
-        graph = generate_child_kozik_graph(graph)
-        self._assign_init_communities(graph)
-        nodes = [node for node in graph.get_nodes()]
-        print('stop')
-        for node in nodes:
-            neighbours = graph.get_neighbours(node)
-            neighbour_to_gain_map: Dict[int, float] = self.map_modularity_gain_to_neighbours(graph, node, neighbours)
-            highest_gain_neighbour = max(neighbour_to_gain_map, key=neighbour_to_gain_map.get)
-            # print(neighbour_to_gain_map[highest_gain_neighbour])
-            if neighbour_to_gain_map[highest_gain_neighbour] > 0: #parametr
-                new_community = graph.get_node_community(highest_gain_neighbour)
-                graph.add_to_community(new_community, node)
-        print('stop')
-        graph = generate_child_kozik_graph(graph)
-        self._assign_init_communities(graph)
-        nodes = [node for node in graph.get_nodes()]
-        print('stop')
-
-    def _assign_init_communities(self, graph: KozikGraph):
-        for community, node in enumerate(graph.get_nodes()):
-            graph.add_to_community(node, community)
-            print('stop')
 
     def map_modularity_gain_to_neighbours(self, graph: KozikGraph, node: int, neighbours: Iterable[int]) -> dict[
         int, float]:
         gain_map = {}
         for neighbour in neighbours:
             neighbour_community = graph.get_node_community(neighbour)
-            # if neighbour_community == graph.get_node_community(node):
-            #     gain_map[neighbour] = -1
-            #     continue
 
             gain_map[neighbour] = self._calculate_modularity_gain(graph, neighbour_community, node)
         return gain_map
 
     def _calculate_modularity_gain(self, graph: KozikGraph, community: int, node: int) -> float:
-        current_community = graph.get_node_community(node)
         Ein = graph.get_edges_inside_community_weight_sum(community)
         Etot = graph.get_edges_incident_to_community_weight_sum(community)
         ki = graph.get_edges_incident_to_node_weight_sum(node)
-        kin = (graph.get_edges_in_community_incident_to_node_weight_sum(node, community)
-               if current_community != community
-               else 1)
+        kin = graph.get_edges_in_community_incident_to_node_weight_sum(node, community)
         m = graph.get_all_edges_weight_sum()
-        print('stop')
         return 1000*((((Ein + kin) / (2 * m)) - (((Etot + ki) / (2 * m)) ** 2)) - (
                     (Ein / (2 * m)) - ((Etot / (2 * m)) ** 2) - ((ki / (2 * m)) ** 2)))
 
