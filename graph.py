3281ecf8faaa21c34aa52fa591491bd05dc893ce
pkoziel1 <74543639+pkoziel1@users.noreply.github.com>
2022-06-22 22:52:49 +0200

    delete self loop with 0 weight

--- graph.py
+++ graph.py
@@ -126,7 +126,8 @@ def generate_child_kozik_graph(kozik_graph: KozikGraph) -> KozikGraph:
     for community in new_nodes:
         edges_inside_community = kozik_graph.get_edges_inside_community(community)
         community_weight_sum = kozik_graph.get_edges_weight_sum(edges_inside_community)
-        G.add_weighted_edges_from([(community, community, community_weight_sum)])
+        if edges_inside_community:
+            G.add_weighted_edges_from([(community, community, community_weight_sum)])
         edges_incident_to_community = kozik_graph.get_edges_incident_to_community(community)
         outside_edges = [edge for edge in edges_incident_to_community if edge not in edges_inside_community]
         for edge in outside_edges:
