311ea365e4ea3d0c0f732ac5f0149211c7d85014
pkoziel1 <74543639+pkoziel1@users.noreply.github.com>
2023-06-15 22:57:13 +0200

    Update sprawdzam.py

--- sprawdzam.py
+++ sprawdzam.py
@@ -7,17 +7,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 
+
 def get_bitcoin_graph():
     data = pd.read_csv("soc-sign-bitcoinalpha.csv")
-    source_list = list(data['source'])[2000:2500]
-    target_list = list(data['target'])[2000:2500]
-    rating_list = list(data['rating'])[2000:2500]
-    print(rating_list)
+    source_list = list(data['source']-1)
+    target_list = list(data['target']-1)
+    rating_list = list(data['rating'])
     G = nx.Graph()
+    myList = []
+    for number in source_list:
+        if number not in myList:
+            myList.append(number)
+    for number in target_list:
+        if number not in myList:
+            myList.append(number)
     G.add_nodes_from(list(range(0, max(data['source']))))
     G.add_weighted_edges_from(list(zip(source_list, target_list, rating_list)))
-    mod = nx_comm.modularity(G, nx_comm.greedy_modularity_communities(G))
-    c = nx_comm.greedy_modularity_communities(G)
+    nx.draw_networkx_labels(G, pos=nx.circular_layout(G))
+    nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
     print('stop')
     return G
 
@@ -40,28 +47,40 @@ def fast_graph():
 
 def ring_graph():
     G = nx.Graph()
-
-    # G.add_nodes_from(range(0, 10))
     G.add_nodes_from(range(0, 150))
     list_of_edges = []
-    # for z in range(0, 2):
     for z in range(0, 30):
         for x in range(z*5, z*5+4):
             for y in range(1, 5):
                 if x + y > z*5+4:
                     continue
                 list_of_edges.append((x, x + y, 1))
-        # if z*5+5 < 10:
         if z*5+5 < 150:
             list_of_edges.append((z*5+4, z*5+5, 1))
     list_of_edges.append((0, 149, 1))
-    # list_of_edges.append((0, 9, 1))
     G.add_weighted_edges_from(list_of_edges)
     nx.draw_networkx_labels(G, pos=nx.circular_layout(G))
     nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
+
+    print(math.ceil((time.perf_counter_ns() - startpoint) / 1000000))
+    print("^ before greedy algorithm")
+
     mod = nx_comm.modularity(G, nx_comm.greedy_modularity_communities(G))
-    c = nx_comm.greedy_modularity_communities(G)
+
+    print(math.ceil((time.perf_counter_ns() - startpoint) / 1000000))
+    print("^ after greedy algorithm")
+
     print(f'NetX mod: {mod}')
+
+    print(math.ceil((time.perf_counter_ns() - startpoint) / 1000000))
+    print("^ before louvain algorithm")
+
+    mod2 = nx_comm.modularity(G, nx_comm.louvain_communities(G))
+
+    print(math.ceil((time.perf_counter_ns() - startpoint) / 1000000))
+    print("^ after louvain algorithm")
+
+    print(f'NetX mod: {mod2}')
     return G
 
 
@@ -83,6 +102,7 @@ def ring_graph_after_1_step():
     print(f'NetX mod: {mod}')
     return G
 
+
 def ring_graph_after_2_step():
     G = nx.Graph()
     G.add_nodes_from(range(0, 15))
@@ -96,34 +116,53 @@ def ring_graph_after_2_step():
     G.add_weighted_edges_from(list_of_edges)
     nx.draw_networkx_labels(G, pos=nx.circular_layout(G))
     nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
-    # plt.show()
+    mod = nx_comm.modularity(G, nx_comm.greedy_modularity_communities(G))
+
+    print(f'NetX mod: {mod}')
+
     return G
+
 
 def karate_graph():
     G = nx.karate_club_graph()
     nx.draw_networkx_labels(G, pos=nx.circular_layout(G))
     nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
+
+    print(math.ceil((time.perf_counter_ns() - startpoint) / 1000000))
+    print("^ before greedy algorithm")
+
     mod = nx_comm.modularity(G, nx_comm.greedy_modularity_communities(G))
-    c = nx_comm.greedy_modularity_communities(G)
-    # plt.show()
+
+    print(math.ceil((time.perf_counter_ns() - startpoint) / 1000000))
+    print("^ after greedy algorithm")
+
     print(f'NetX mod: {mod}')
+
+    print(math.ceil((time.perf_counter_ns() - startpoint) / 1000000))
+    print("^ before louvain algorithm")
+
+    mod2 = nx_comm.modularity(G, nx_comm.louvain_communities(G))
+
+    print(math.ceil((time.perf_counter_ns() - startpoint) / 1000000))
+    print("^ after louvain algorithm")
+
+    print(f'NetX mod: {mod2}')
     return G
 
+
 def florentine_families_graph():
-    # G = nx.florentine_families_graph()
-    # G = nx.davis_southern_women_graph()
-    G = nx.les_miserables_graph()
+    G = nx.florentine_families_graph()
     mapping = {old_name: idx for idx, old_name in enumerate(G.nodes())}
     for u, v, d in G.edges(data=True):
         d['weight'] = 1
     G = nx.relabel_nodes(G, mapping)
     nx.draw_networkx_labels(G, pos=nx.circular_layout(G))
     nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
+
     mod = nx_comm.modularity(G, nx_comm.greedy_modularity_communities(G))
-    c = nx_comm.greedy_modularity_communities(G)
-    # plt.show()
     print(f'NetX mod: {mod}')
     return G
+
 
 def small_graph():
     G = nx.Graph()
@@ -134,8 +173,6 @@ def small_graph():
     nx.draw_networkx_labels(G, pos=nx.circular_layout(G))
     nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
     mod = nx_comm.modularity(G, nx_comm.greedy_modularity_communities(G))
-    c = nx_comm.greedy_modularity_communities(G)
-    # plt.show()
     print(f'NetX mod: {mod}')
     return G
 
@@ -148,15 +185,20 @@ def small_graph():
 # graph = KozikGraph(graph=ring_graph())
 # graph = KozikGraph(graph=ring_graph_after_1_step())
 # graph = KozikGraph(graph=ring_graph_after_2_step())
-startpoint = time.perf_counter_ns()
-print("startpoint = 0")
-graph = KozikGraph(graph=florentine_families_graph())
-fast_unfolding = FastUnfolding()
-print("checkpoint1 (after class load) = ")
-print(math.ceil((time.perf_counter_ns() - startpoint)/1000000))
-fast_unfolding.run(graph)
-print("checkpoint2 (after fast_unfolding) = ")
-print(math.ceil((time.perf_counter_ns() - startpoint)/1000000))
-plt.show()
 
-print('stoped')
+print("startpoint = 0")
+startpoint = time.perf_counter_ns()
+
+graph = KozikGraph(graph=karate_graph())
+
+fast_unfolding = FastUnfolding()
+
+print(math.ceil((time.perf_counter_ns() - startpoint)/1000000))
+print("^ before fast_unfolding")
+
+fast_unfolding.run(graph)
+
+print(math.ceil((time.perf_counter_ns() - startpoint)/1000000))
+print("^ after fast_unfolding")
+
+plt.show()
